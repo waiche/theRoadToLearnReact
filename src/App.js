39c23e3943206ec466066efaa18b558b1b586e7d
waiche <CHbjRv7aZtog>
2019-03-06 01:10:05 +0100

    - Chapter : Paginated Fetch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
 
 console.log(url);
 
@@ -29,8 +32,8 @@ class App extends Component {
         this.onDismiss = this.onDismiss.bind(this);
     }
 
-    fetchSearchTopStories(searchTerm) {
-        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    fetchSearchTopStories(searchTerm, page = 0) {
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
             .then(response => response.json())
             .then(result => this.setSearchTopStories(result))
             .catch(error => error);
@@ -43,7 +46,17 @@ class App extends Component {
     }
 
     setSearchTopStories(result) {
-        this.setState({result});
+        const {hits, page } = result;
+        const oldHits = page !== 0
+            ? this.state.result.hits
+            : [];
+        const  updateHits = [
+            ...oldHits,
+            ...hits
+        ];
+        this.setState({
+            result: {hits: updateHits, page}
+        });
     }
 
     onSearchChange(event) {
@@ -68,9 +81,8 @@ class App extends Component {
 
     render() {
         const {searchTerm, result} = this.state;
-        if (!result) {
-            return null;
-        }
+        const page = (result && result.page) || 0;
+
         return (
             <div className="page">
                 <div className="interactions">
@@ -88,6 +100,11 @@ class App extends Component {
                         onDismiss={this.onDismiss}
                     />
                 }
+                <div className="interactions">
+                    <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+                        More
+                    </Button>
+                </div>
             </div>
 
         )
