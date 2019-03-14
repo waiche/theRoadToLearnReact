48915dbc786b71376ff1573c7268941fe8d5f11b
waiche <CHbjRv7aZtog>
2019-03-15 00:20:31 +0100

    - Chapter : Component interface with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -175,6 +176,13 @@ const Search = ({value, onChange, onSubmit, children}) => {
     )
 }
 
+Search.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    onSubmit: PropTypes.func,
+    children: PropTypes.node,
+}
+
 const largeColum = {
     width: '40%',
 };
@@ -215,8 +223,23 @@ const Table = ({list, onDismiss}) =>
             </div>)}
     </div>
 
-const Button = ({onClick, className = '', children}) => {
-    return (
+Table.propTypes = {
+    list: PropTypes.arrayOf(
+      PropTypes.shape(({
+         objectID: PropTypes.string.isRequired,
+         author: PropTypes.string,
+         url: PropTypes.string,
+         num_comments: PropTypes.number,
+         points: PropTypes.number,
+      }))
+    ),
+    onDismiss: PropTypes.func.isRequired,
+}
+
+const Button = ({
+        onClick,
+        className,
+        children}) =>
         <button
             onClick={onClick}
             className={className}
@@ -224,7 +247,15 @@ const Button = ({onClick, className = '', children}) => {
         >
             {children}
         </button>
-    )
+
+Button.defaultProps = {
+    className: '',
+}
+
+Button.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    className: PropTypes.string,
+    children: PropTypes.node.isRequired,
 }
 
 export default App;
